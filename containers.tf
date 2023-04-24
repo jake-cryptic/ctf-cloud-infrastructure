477ae3fc8b6a72b531886ac684490453dd964232
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2023-04-24 03:04:37 +0100

    Terraform, why do you keep telling me of errors? 🥲

--- containers.tf
+++ containers.tf
@@ -1,11 +1,11 @@
 data "azurerm_key_vault_secret" "registry-password" {
   name         = "registry-password"
-  key_vault_id = azurerm_key_vault.rctf-keyvault.id
+  key_vault_id = azurerm_key_vault.rctf-main-keyvault.id
 }
 
 data "azurerm_key_vault_secret" "openai-api-key" {
   name         = "openai-api-key"
-  key_vault_id = azurerm_key_vault.rctf-keyvault.id
+  key_vault_id = azurerm_key_vault.rctf-main-keyvault.id
 }
 
 
--- keyvault.tf
+++ keyvault.tf
@@ -1,16 +1,16 @@
-resource "azurerm_key_vault" "rctf-keyvault" {
-  name                       = "rctf-keyvault"
+resource "azurerm_key_vault" "rctf-main-keyvault" {
+  name                       = "rctf-main-keyvault"
   location                   = azurerm_resource_group.rctf.location
   resource_group_name        = azurerm_resource_group.rctf.name
   tenant_id                  = "bdb74b30-9568-4856-bdbf-06759778fcbc"
-  soft_delete_retention_days = 7
-  purge_protection_enabled   = true
+  soft_delete_retention_days = 90
+  purge_protection_enabled   = false
 
   sku_name = "standard"
 }
 
-resource "azurerm_key_vault_access_policy" "rctf-keyvault-ap" {
-  key_vault_id = azurerm_key_vault.rctf-keyvault.id
+resource "azurerm_key_vault_access_policy" "rctf-main-keyvault-ap" {
+  key_vault_id = azurerm_key_vault.rctf-main-keyvault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
@@ -27,11 +27,11 @@ resource "azurerm_key_vault_access_policy" "rctf-keyvault-ap" {
   ]
 }
 
-resource "azurerm_key_vault_secret" "rctf-keyvault-secrets" {
-  depends_on   = [azurerm_key_vault.rctf-keyvault]
+resource "azurerm_key_vault_secret" "rctf-main-keyvault-secrets" {
+  depends_on   = [azurerm_key_vault.rctf-main-keyvault]
   for_each     = local.key_vault_secret
   name         = each.value.name
-  key_vault_id = azurerm_key_vault.rctf-keyvault.id
+  key_vault_id = azurerm_key_vault.rctf-main-keyvault.id
   value        = each.value.value
 
   lifecycle {
