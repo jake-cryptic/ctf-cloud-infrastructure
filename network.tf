0b2ebc2a749c7a3dc552b3f89f7a246f0edc9bc9
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2023-04-29 06:43:43 +0100

    fix2

--- network.tf
+++ network.tf
@@ -52,37 +52,37 @@ resource "azurerm_network_security_group" "rctf-main-nsg" {
   resource_group_name = azurerm_resource_group.rctf.name
 
   security_rule {
-    name                       = "allow-ssh"
+    name                       = "allow-all"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_range     = "*"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "allow-http"
-    priority                   = 200
+    name                       = "allow-all-except-docker"
+    priority                   = 400
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "80"
+    destination_port_ranges    = ["0-2374", "2377-65535"]
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "allow-https"
-    priority                   = 300
+    name                       = "allow-udp-ports-above-2377"
+    priority                   = 500
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"
+    protocol                   = "Udp"
     source_port_range          = "*"
-    destination_port_range     = "443"
+    destination_port_range     = "2378-65535"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
